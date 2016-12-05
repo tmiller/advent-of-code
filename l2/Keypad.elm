86d217ab716e0486235a4dfa32d9e5af50720a7a
Tom Miller <tmiller@eab.com>
2016-12-04 19:55:09 -0600

    Solve level 2

--- l2/Keypad.elm
+++ l2/Keypad.elm
@@ -1,31 +1,173 @@
-module Keypad exposing (Model, Msg, init, update, subscriptions, view)
+module Keypad exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Array
 
 
 type alias Model =
-    { code : List String
-    , key : List Int
+    { answer1 : String
+    , answer2 : String
     }
 
 
-initModel : Model
-initModel =
-    { code = actualCode
-    , key = [ 5 ]
+type alias Key =
+    String
+
+
+type alias Position =
+    ( Int, Int )
+
+
+type alias Keypad =
+    Array.Array (Array.Array (Maybe Key))
+
+
+type Direction
+    = Up
+    | Down
+    | Left
+    | Right
+
+
+normalKeypad : Keypad
+normalKeypad =
+    Array.fromList
+        [ Array.fromList [ Nothing, Nothing, Nothing, Nothing, Nothing ]
+        , Array.fromList [ Nothing, Just "1", Just "2", Just "3", Nothing ]
+        , Array.fromList [ Nothing, Just "4", Just "5", Just "6", Nothing ]
+        , Array.fromList [ Nothing, Just "7", Just "8", Just "9", Nothing ]
+        , Array.fromList [ Nothing, Nothing, Nothing, Nothing, Nothing ]
+        ]
+
+
+advancedKeypad : Keypad
+advancedKeypad =
+    Array.fromList
+        [ Array.fromList [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+        , Array.fromList [ Nothing, Nothing, Nothing, Just "1", Nothing, Nothing, Nothing ]
+        , Array.fromList [ Nothing, Nothing, Just "2", Just "3", Just "4", Nothing, Nothing ]
+        , Array.fromList [ Nothing, Just "5", Just "6", Just "7", Just "8", Just "9", Nothing ]
+        , Array.fromList [ Nothing, Nothing, Just "A", Just "B", Just "C", Nothing, Nothing ]
+        , Array.fromList [ Nothing, Nothing, Nothing, Just "D", Nothing, Nothing, Nothing ]
+        , Array.fromList [ Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing ]
+        ]
+
+
+model : Model
+model =
+    { answer1 = ""
+    , answer2 = ""
     }
+
+
+parseDirection : Char -> Maybe Direction
+parseDirection c =
+    case c of
+        'U' ->
+            Just Up
+
+        'R' ->
+            Just Right
+
+        'D' ->
+            Just Down
+
+        'L' ->
+            Just Left
+
+        _ ->
+            Nothing
+
+
+parseDirections : String -> List Direction
+parseDirections =
+    String.toList >> List.filterMap parseDirection
+
+
+parseCode : List String -> List (List Direction)
+parseCode =
+    List.map parseDirections
+
+
+solve : Keypad -> List String -> Position -> String
+solve keypad code position =
+    List.scanl (solveDigit keypad) position (parseCode code)
+        |> List.map (getKey keypad)
+        |> List.filterMap identity
+        |> List.tail
+        |> answerToString
+
+
+solveDigit : Keypad -> List Direction -> Position -> Position
+solveDigit keypad directions startPosition =
+    List.foldl (movePosition keypad) startPosition directions
+
+
+movePosition : Keypad -> Direction -> Position -> Position
+movePosition keypad direction position =
+    let
+        nextPosition =
+            getNextPosition position direction
+    in
+        case getKey keypad nextPosition of
+            Just key ->
+                nextPosition
+
+            Nothing ->
+                position
+
+
+getNextPosition : Position -> Direction -> Position
+getNextPosition position direction =
+    case direction of
+        Up ->
+            Tuple.mapSecond ((+) -1) position
+
+        Right ->
+            Tuple.mapFirst ((+) 1) position
+
+        Down ->
+            Tuple.mapSecond ((+) 1) position
+
+        Left ->
+            Tuple.mapFirst ((+) -1) position
+
+
+getKey : Keypad -> Position -> Maybe Key
+getKey keypad position =
+    case Array.get (Tuple.second position) keypad of
+        Just row ->
+            case Array.get (Tuple.first position) row of
+                Just key ->
+                    key
+
+                Nothing ->
+                    Nothing
+
+        Nothing ->
+            Nothing
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( initModel, Cmd.none )
+    ( model, Cmd.none )
 
 
 type Msg
     = NoOp
-    | Step
+    | Solve
+
+
+answerToString : Maybe (List String) -> String
+answerToString answer =
+    case answer of
+        Just ans ->
+            String.concat ans
+
+        Nothing ->
+            "No Answer"
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -34,74 +176,15 @@ update msg model =
         NoOp ->
             ( model, Cmd.none )
 
-        Step ->
+        Solve ->
             let
-                solvedDigits =
-                    List.foldl solveDigit model.key model.code
+                answer1 =
+                    solve normalKeypad actualCode ( 2, 2 )
 
-                digits =
-                    List.reverse solvedDigits
+                answer2 =
+                    solve advancedKeypad actualCode ( 1, 3 )
             in
-                ( { model
-                    | key = digits
-                    , code = []
-                  }
-                , Cmd.none
-                )
-
-
-solveDigit : String -> List Int -> List Int
-solveDigit codeChunk seed =
-    case seed of
-        x :: xs ->
-            List.foldl moveState x (String.toList codeChunk) :: x :: xs
-
-        [] ->
-            []
-
-
-moveState : Char -> Int -> Int
-moveState dir digit =
-    let
-        moveUp =
-            if digit > 3 then
-                digit - 3
-            else
-                digit
-
-        moveRight =
-            if digit % 3 == 0 then
-                digit
-            else
-                digit + 1
-
-        moveDown =
-            if digit < 7 then
-                digit + 3
-            else
-                digit
-
-        moveLeft =
-            if List.any ((==) digit) [ 1, 4, 7 ] then
-                digit
-            else
-                digit - 1
-    in
-        case dir of
-            'U' ->
-                moveUp
-
-            'R' ->
-                moveRight
-
-            'D' ->
-                moveDown
-
-            'L' ->
-                moveLeft
-
-            _ ->
-                digit
+                ( { model | answer1 = answer1, answer2 = answer2 }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -113,10 +196,10 @@ view : Model -> Html Msg
 view model =
     div []
         [ div []
-            [ button [ onClick Step ] [ text "Step" ]
+            [ button [ onClick Solve ] [ text "Solve" ]
             ]
-        , viewCode model.code
-        , viewKey model.key
+        , div [] [ text ("Answer 1: " ++ model.answer1) ]
+        , div [] [ text ("Answer 2: " ++ model.answer2) ]
         ]
 
 
@@ -127,16 +210,6 @@ viewCode code =
             List.map (\x -> li [] [ text x ]) code
     in
         ul [] codeItems
-
-
-viewKey : List Int -> Html Msg
-viewKey code =
-    case code of
-        x :: xs ->
-            ul [] (List.map (\y -> li [] [ text (toString y) ]) xs)
-
-        [] ->
-            ul [] []
 
 
 testCode : List String
