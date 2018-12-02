8c3054f9b7ac02e33e905eca27b3d83dd326c83f
Tom Miller <jackerran@gmail.com>
2018-12-02 03:59:51 +0000

    2018 Day 1 Refactor Parser to use Megaparsec

--- 2018/src/Day1.hs
+++ 2018/src/Day1.hs
@@ -1,30 +1,25 @@
 module Day1 where
 
-import Text.Trifecta
-import Data.Monoid
-import Data.Maybe (fromMaybe)
-import Data.Set as S
 import Data.Bool (bool)
+import Data.Monoid
+import Data.Set as S
+import Lib
+import Text.Megaparsec
 
 input :: String
 input = "data/day1.txt"
 
 part1 :: IO String
-part1 =  mconcat <$> getNumbers >>= display
+part1 = eitherPE (show . getSum . mconcat) <$> parseFile
 
 part2 :: IO String
-part2 = findDuplicate <$> scan <$> cycle <$> getNumbers >>= display
+part2 = eitherPE (show . getSum . findDuplicate . scan . cycle) <$> parseFile
   where scan = scanl (<>) (Sum 0)
 
-getNumbers :: IO [Sum Integer]
-getNumbers = fromMaybe [] <$> parseFromFile p input
-  where p = ((Sum <$> integer) `sepBy` whiteSpace) <* eof
-
+parseFile :: IO (Either ParseErrorBundle [Sum Integer])
+parseFile = parse (many (Sum <$> integer) <* eof) input <$> readFile input
 
 findDuplicate :: (Monoid a, Ord a) => [a] -> a
 findDuplicate xs = go xs S.empty
   where go []      _    = mempty
         go (x:xs') seen = bool (go xs' (S.insert x seen)) x (S.member x seen)
-
-display :: Sum Integer -> IO String
-display = return . show . getSum
--- 2018/src/Lib.hs
+++ 2018/src/Lib.hs
@@ -1,6 +1,15 @@
 module Lib ( (|>)
            , (||>)
+           , Parser
+           , ParseErrorBundle
+           , eitherPE
+           , integer
            ) where
+
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char as MC
+import qualified Text.Megaparsec.Char.Lexer as L
 
 infixl 0 |>
 infixl 4 ||>
@@ -10,3 +19,15 @@ infixl 4 ||>
 
 (||>) :: Functor f => f a -> (a -> b) -> f b
 (||>) = flip (<$>)
+
+-- Parsers
+
+type Parser = Parsec Void String
+type ParseErrorBundle = ParseError (Token String) Void
+
+integer :: Parser Integer
+integer = L.signed space (L.lexeme space L.decimal)
+
+eitherPE :: (ShowToken t, ShowErrorComponent e, Ord t) =>
+  (b -> String) -> Either (ParseError t e) b -> String
+eitherPE = either parseErrorPretty
